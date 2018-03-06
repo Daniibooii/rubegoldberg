24a08dd42f81bb9fd80dd9aee7380cf86f87692e
Xhandler <chandlerd@gmail.com>
2018-03-05 19:48:44 -0500

    Cleaned up repo, commented everything. Along with minor revisions

--- entities.js
+++ entities.js
@@ -1,15 +1,35 @@
-// TODO: Here we will add the the entities being used by the game
-function AddBox(x, y, w, h) {
-	this.body = Bodies.rectangle(x, y, w, h)
+
+/*****************************************************************************************
+** 							Here we will add constuctors to build our entities 											**
+** Expect each constuctor to have a options object, here we will edit matter properties **
+** 			Each constructor must have a show method to render the object with P5.js 				**
+** Matter renders bodies at the center point P5's default is to render starting top left**
+**					Easist work around is setting the mode to CENTER see ln 31 									**
+******************************************************************************************/
+
+
+
+function AddBox(x, y, w, h) { // Ex. Constructor
+	let options = { // Ex. options obj
+		// TODO: Add every potential propety + desciption? 
+		friction: .2,
+		restitution: 1
+	}
+	this.body = Bodies.rectangle(x, y, w, h, options) // See matter.js docs for more bodies
 	World.add(world, this.body);
 	this.w = w;
 	this.h = h;
-	this.show = () => {
+	this.show = () => { // Ex. Show method XXX Caution depending on the entity shape / appearance these must be altered
 		let pos = this.body.position;
 		let angle = this.body.angle;
 
 		push();
 		translate(pos.x, pos.y);
+		rotate(angle);
+		strokeWeight(1);
+		stroke(0);
+		fill(255);
+		rectMode(CENTER); // align P5 with matter.js
 		rect(0, 0, this.w, this.h);
 		pop();
 	}
@@ -17,7 +37,11 @@ function AddBox(x, y, w, h) {
 }
 
 function AddCircle(x, y, r) {
-	this.body = Bodies.circle(x, y, r)
+	let options = {
+		friction: .2,
+		restitution: 1
+	}
+	this.body = Bodies.circle(x, y, (r/2), options)
 	World.add(world, this.body);
 	this.r = r;
 	this.show = () => {
@@ -26,6 +50,10 @@ function AddCircle(x, y, r) {
 
 		push();
 		translate(pos.x, pos.y);
+		rotate(angle);
+		strokeWeight(1);
+		stroke(222);
+		fill(120);
 		ellipse(0, 0, this.r, this.r);
 		pop();
 	}
--- mattest.js
+++ mattest.js
@@ -1,5 +1,12 @@
+/*********************************************************
+**	Here we have the functionality to load our entities **
+** 								UNDER CONSTRUCTION										**
+**********************************************************/
+
+
 let selected = "circle";
 
+// Importing Matter.js
 let Engine = Matter.Engine,
 	Render = Matter.Render,
 	World = Matter.World,
@@ -9,38 +16,57 @@ let engine;
 let boxes = [];
 let circles = [];
 let world;
-// let options ={
-//   mouse: mouse
-// }
-function setup() {
-  createCanvas(640, 640);
-  engine = Engine.create();
-  world = engine.world;
-  Engine.run(engine);
-  console.log(world);
-  let = ground = Bodies.rectangle(400, 610, 810, 60, {
-      isStatic: true
-  });
-  World.add(world, ground);
-}
-function mouseDragged(){
-  //Here we are using our constructor to add new boxes. see entities.js
-  if(selected === "box"){
-  boxes.push(new AddBox(mouseX, mouseY, 80, 80));
-  console.log(boxes[1])
-} else if(selected === "circle"){
-  boxes.push(new AddCircle(mouseX, mouseY, 40))
-}else{
-  console.log("nothing selected");
+let options = {
+	isStatic: true
 }
 
+function setup() {
+	//TODO: Add some sort of level system 
+	//Draw the area our game will be played then center
+	let cnv = createCanvas(1120, 640);
+	let x = (windowWidth - width) / 2;
+	let y = (windowHeight - height) / 2;
+	cnv.position(x, y);
+
+	//Create our physics engine
+	engine = Engine.create();
+	world = engine.world;
+	Engine.run(engine);
+	let ground = Bodies.rectangle(560, height, width, 30, options);
+	World.add(world, ground);
 }
+
+function mouseDragged() {
+	// TODO: Revise this found and conditional to better suite our game. Possibly think of way around conditional statement
+	//Here we are using our constructor to add new boxes. see entities.js
+	if (selected === "box") {
+		boxes.push(new AddBox(mouseX, mouseY, (random(16) * 8), (random(16) * 8)));
+	} else if (selected === "circle") {
+		circles.push(new AddCircle(mouseX, mouseY, random(64)))
+	} else {
+		console.log("nothing selected");
+	}
+
+}
+
 function draw() {
-	background(51);
-  for (var i = 0; i < circles.length; i++) {
-    circles[i].show();
-  }
-  for (var i = 0; i < boxes.length; i++) {
-    boxes[i].show();
-  }
+	// TODO: Remove for loops and use map to draw all of our entities
+	// Here is where all of our rendering does down
+	background(187);
+	for (var i = 0; i < circles.length; i++) {
+		circles[i].show();
+	}
+	// let drawCircles = circles.map(()=>{
+	// 	this.show});
+
+	for (var i = 0; i < boxes.length; i++) {
+		boxes[i].show();
+	}
+	//Ground
+	push();
+	rectMode(CENTER); // Mode center set
+	rect(560, height, width, 30);
+	fill(100);
+	pop();
+
 }
